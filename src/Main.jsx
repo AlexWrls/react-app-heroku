d8b069b1259084b61f86ab1511181763e6f39c23
Aleksandr <atrof86@yandex.ru>
2022-02-10 19:46:34 +0700

    commit

--- src/Main.jsx
+++ src/Main.jsx
@@ -4,27 +4,29 @@ const Main = () => {
     const [data, setData] = useState([]);
     const [isLoading, setLoading] = useState(true);
 
-    useEffect(() => {
-        fetch('/main')
-            .then((res) => res.json())
-            .then(
-                (data) => {
-                    setData(data);
-                    console.log(data);
-                    setLoading(false);
-                },
-                (error) => {
-                    console.log(error);
-                    setLoading(false);
-                }
-            );
-    }, []);
-
+    const createFetch=()=>{
+        useEffect(() => {
+            fetch('/https://simple-react-back.herokuapp.com/main')
+                .then((res) => res.json())
+                .then(
+                    (data) => {
+                        setData(data);
+                        console.log(data);
+                        setLoading(false);
+                    },
+                    (error) => {
+                        console.log(error);
+                        setLoading(false);
+                    }
+                );
+        }, []);
+    }
     if (isLoading) {
         return <h1>Loading data... </h1>;
     } else {
         return (
             <div className="card">
+                <button onClick={createFetch} className="btn-sm btn-primary">create</button>
                 <div className="card-body">
                     <table className="table table-hover">
                         <thead>
